2e64e4403386ac9893124f4e182fb1312ceb17d1
Ankit Kumar <ankit512.kumar@gmail.com>
2020-07-20 04:06:13 +0530

    Java Project

---
+++ src/PeriodicTable/PeriodicTable.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PeriodicTable;
+/**
+ *
+ * @author Ankit
+ */
+public class PeriodicTable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new intro().setVisible(true);
+        // TODO code application logic here
+    }
+}
